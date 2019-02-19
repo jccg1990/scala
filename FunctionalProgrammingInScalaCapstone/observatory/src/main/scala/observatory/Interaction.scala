e7c7e14225d8ef31b428134812845a237e170353
jccg1990 <jccg1990@gmail.com>
2019-02-18 23:57:23 -0700

    Adds FPISC weeks 2 and 3

--- FunctionalProgrammingInScalaCapstone/observatory/src/main/scala/observatory/Interaction.scala
+++ FunctionalProgrammingInScalaCapstone/observatory/src/main/scala/observatory/Interaction.scala
@@ -1,6 +1,9 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import observatory.Visualization.{interpolateColor, predictTemperature}
+
+import scala.math._
 
 /**
   * 3rd milestone: interactive visualization
@@ -8,35 +11,55 @@ import com.sksamuel.scrimage.{Image, Pixel}
 object Interaction {
 
   /**
+    * @param temperatures Known temperatures
+    * @param colors       Color scale
+    * @param tile         Tile coordinates
+    * @return A 256×256 image showing the contents of the given tile
+    */
+  def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
+    def positionToLocation(p: Int) = {
+      val x1 = 256 * tile.x + p % 256
+      val y1 = 256 * tile.y + p / 256
+
+      tileLocation(Tile(x1, y1, tile.zoom + 8))
+    }
+
+    val pixels = (0 until 256 * 256).par.map(p => {
+      val t = predictTemperature(temperatures, positionToLocation(p))
+      val c = interpolateColor(colors, t)
+      Pixel(c.red, c.green, c.blue, 127)
+    })
+
+    Image(256, 256, pixels.toArray)
+  }
+
+  /**
     * @param tile Tile coordinates
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
   def tileLocation(tile: Tile): Location = {
-    ???
-  }
+    def toLocation(x: Int, y: Int, z: Int) = Location(
+      toDegrees(atan(sinh(Pi * (1.0 - 2.0 * y.toDouble / (1 << z))))),
+      x.toDouble / (1 << z) * 360.0 - 180.0)
 
-  /**
-    * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @param tile Tile coordinates
-    * @return A 256×256 image showing the contents of the given tile
-    */
-  def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
-    ???
+    toLocation(tile.x, tile.y, tile.zoom)
   }
 
   /**
     * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
-    * @param yearlyData Sequence of (year, data), where `data` is some data associated with
-    *                   `year`. The type of `data` can be anything.
+    *
+    * @param yearlyData    Sequence of (year, data), where `data` is some data associated with
+    *                      `year`. The type of `data` can be anything.
     * @param generateImage Function that generates an image given a year, a zoom level, the x and
     *                      y coordinates of the tile and the data to build the image from
     */
-  def generateTiles[Data](
-    yearlyData: Iterable[(Year, Data)],
-    generateImage: (Year, Tile, Data) => Unit
-  ): Unit = {
-    ???
+  def generateTiles[Data](yearlyData: Iterable[(Year, Data)], generateImage: (Year, Tile, Data) => Unit): Unit = {
+    for {
+      z <- 0 to 3
+      x <- 0 until 1 << z
+      y <- 0 until 1 << z
+      (year, d) <- yearlyData
+    } yield generateImage(year, Tile(x, y, z), d)
   }
 
 }
--- FunctionalProgrammingInScalaCapstone/observatory/src/main/scala/observatory/Visualization.scala
+++ FunctionalProgrammingInScalaCapstone/observatory/src/main/scala/observatory/Visualization.scala
@@ -2,36 +2,108 @@ package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
 
+import scala.math._
+
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
 
+  val p: Double = 2.0
+  val earthRadius: Double = 6371.0088
+
+  /**
+    * @param temperatures Known temperatures
+    * @param colors       Color scale
+    * @return A 360×180 image where each pixel shows the predicted temperature at its location
+    */
+  def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
+    val pixels = (0 until 360 * 180).par.map(p => {
+      val t = predictTemperature(temperatures, convertToLocation(p))
+      val c = interpolateColor(colors, t)
+      Pixel(c.red, c.green, c.blue, 255)
+    })
+
+    Image(360, 180, pixels.toArray)
+  }
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    def u: (Double, Double) = temperatures.par.map { case (l, t) =>
+      def d: Double = calculateDistance(l, location)
+
+      if (d < 1) {
+        return (t, 1.0)
+      } else {
+        def wi: Double = 1.0 / pow(d, p)
+
+        (wi * t, wi)
+      }
+    }.reduce((x, y) => (x._1 + y._1, x._2 + y._2))
+
+    u._1 / u._2
+  }
+
+  def calculateDistance(x: Location, y: Location): Double = {
+    def a = if (x.lat == y.lat && x.lon == y.lon) {
+      0.0
+    } else if (x.lat == -y.lat && (x.lon == y.lon - 180 || x.lon == y.lon + 180)) {
+      math.Pi
+    } else {
+      val (la1, lo1) = (x.lat.toRadians, x.lon.toRadians)
+      val (la2, lo2) = (y.lat.toRadians, y.lon.toRadians)
+
+      acos(sin(la1) * sin(la2) + cos(la1) * cos(la2) * cos(abs(lo1 - lo2)))
+    }
+
+    a * earthRadius
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+    def findValue(points: Iterable[(Temperature, Color)]): Color = {
+      points match {
+        case x1 :: x2 :: _ =>
+          if (x1._1 < value && x2._1 > value) {
+            val r = lerp(x1._1, x2._1, x1._2.red, x2._2.red, value)
+            val g = lerp(x1._1, x2._1, x1._2.green, x2._2.green, value)
+            val b = lerp(x1._1, x2._1, x1._2.blue, x2._2.blue, value)
+
+            Color(r, g, b)
+          } else {
+            findValue(points.tail)
+          }
+      }
+    }
+
+    val sorted = points.toList.sortBy(p => p._1)
+
+    if (value <= sorted.head._1) {
+      sorted.head._2
+    } else if (value >= sorted.last._1) {
+      sorted.last._2
+    } else {
+      findValue(sorted)
+    }
   }
 
-  /**
-    * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @return A 360×180 image where each pixel shows the predicted temperature at its location
-    */
-  def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
-    ???
+  def lerp(t0: Double, t1: Double, c0: Int, c1: Int, t: Double): Int = {
+    min(255, (c0 + (t - t0) * (c1 - c0) / (t1 - t0)).round).toInt
+  }
+
+  def convertToLocation(idx: Int): Location = {
+    val latitude: Int = ((idx / 360) * -1) + 90
+    val longitude: Int = (idx % 360) - 180
+
+    Location(latitude, longitude)
   }
 
 }
